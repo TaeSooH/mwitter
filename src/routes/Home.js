1eb13eed56af69181c228ba952c1fd3f6543dfa3
soohyang136 <jsh515228@gmail.com>
2022-06-19 23:57:41 +0900

    get nwitte to database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
 import { dbService } from "myBase";
-import React, { useState } from "react";
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc, serverTimestamp, getDocs, query, getDocsFromCache } from "firebase/firestore";
 const Home = () => {
     const [mweet, setMweet] = useState("");
+    const [mweets, setMweets] = useState([]);
+    const getMweets = async() => {
+        const q = query(collection(dbService, "mweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            const mweetObj = {
+                ...doc.data(),
+                id: doc.id,
+            };
+            console.log(doc.id, " => ", doc.data());
+            setMweets(prev => [mweetObj, ...prev]);
+        });
+    }
+    useEffect( () => {
+        getMweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await addDoc(collection(dbService, "mweets"), {
@@ -21,6 +37,13 @@ const Home = () => {
                 <input value={mweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
                 <input type="submit" value="Mweet" />
             </form>
+            <div>
+                {mweets.map( (mweet) => (
+                    <div key={mweet.id}>
+                        <h4>{mweet.mweet}</h4>
+                    </div>
+                ))};
+            </div>
         </div>
     )
 };
